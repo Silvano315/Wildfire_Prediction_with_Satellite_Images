24d4443ee11e9b30482e8220ac9472cdea59ed04
Silvano Quarto <silvano.quarto@gmail.com>
2024-06-19 19:55:03 +0200

    data viz fx integration

--- src/data_viz_functions.py
+++ src/data_viz_functions.py
@@ -4,6 +4,7 @@ import random
 import cv2
 import os
 import pandas as pd
+import numpy as np
 
 
 # Plot randomized images for each folder and for each label
@@ -66,3 +67,53 @@ def compute_sharpness(image_path):
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     sharpness = cv2.Laplacian(image, cv2.CV_64F).var()
     return sharpness
+
+
+# Define the sharpen function
+def sharpen(image):
+
+    kernel = np.array([[0, -1, 0],
+                       [-1, 5,-1],
+                       [0, -1, 0]])
+    sharpened_image = cv2.filter2D(image, -1, kernel)
+
+    return sharpened_image
+
+
+# Define the noise reduction function
+def noise_reduction(image, kernel_size=(5, 5)):
+
+    blurred_image = cv2.GaussianBlur(image, kernel_size, 0)
+    
+    return blurred_image
+
+
+# Function to load a random image 
+def load_random_image(images_path):
+
+    splits = ['train', 'test', 'valid']
+    categories = ['wildfire', 'nowildfire']
+
+    random_split = random.choice(splits)
+    random_category = random.choice(categories)
+
+    image_dir = os.path.join(images_path, random_split, random_category)
+    images = os.listdir(image_dir)
+    random_image_name = random.choice(images)
+    image_path = os.path.join(image_dir, random_image_name)
+    
+    return cv2.imread(image_path)
+
+# Function to display images (original vs enhanced)
+def display_images(original_image, enhanced_image, title):
+
+    fig, axs = plt.subplots(1, 2, figsize=(12, 6))
+
+    axs[0].imshow(cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
+    axs[0].set_title('Original Image')
+    axs[0].axis('off')
+    axs[1].imshow(cv2.cvtColor(enhanced_image, cv2.COLOR_BGR2RGB))
+    axs[1].set_title(title)
+    axs[1].axis('off')
+    
+    plt.show()
