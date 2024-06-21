ef6479b36b835049e4307258fc2b3c88d00bb951
Silvano Quarto <silvano.quarto@gmail.com>
2024-06-21 16:54:12 +0200

    models functions integration

--- src/models.py
+++ src/models.py
@@ -1,6 +1,9 @@
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 from tensorflow.keras.optimizers import Adam
+from sklearn.metrics import classification_report, confusion_matrix
+import seaborn as sns
+import numpy as np
 
 from src.constants import IMAGE_SIZE, CHANNELS
 
@@ -44,3 +47,63 @@ def train_cnn_model(model, train_generator, validation_generator, epochs=20, ste
     )
 
     return history
+
+
+# Models evaluation
+import matplotlib.pyplot as plt
+
+def plot_training_validation_history(history):
+
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    epochs = range(1, len(acc) + 1)
+    
+    plt.figure(figsize=(14, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(epochs, acc, 'bo', label='Training Accuracy')
+    plt.plot(epochs, val_acc, 'b', label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    
+    plt.subplot(1, 2, 2)
+    plt.plot(epochs, loss, 'ro', label='Training Loss')
+    plt.plot(epochs, val_loss, 'r', label='Validation Loss')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    
+    plt.tight_layout()
+    plt.show()
+
+
+# Function to compare training and test evaluation metrics
+def evaluate_train_test_performance(model, train_generator, test_generator):
+
+    train_loss, train_accuracy = model.evaluate(train_generator, steps=train_generator.samples // train_generator.batch_size)
+    print(f'Train Accuracy: {train_accuracy * 100:.2f}%')
+    print(f'Train Loss: {train_loss:.4f}')
+
+    test_loss, test_accuracy = model.evaluate(test_generator, steps=test_generator.samples // test_generator.batch_size)
+    print(f'Test Accuracy: {test_accuracy * 100:.2f}%')
+    print(f'Test Loss: {test_loss:.4f}')
+
+    test_generator.reset()
+    y_true = test_generator.classes
+    y_pred = model.predict(test_generator, steps=test_generator.samples // test_generator.batch_size)
+    y_pred = np.rint(y_pred).astype(int).flatten()
+
+    print("\nClassification Report for Test Set:")
+    print(classification_report(y_true, y_pred, target_names=test_generator.class_indices.keys()))
+
+    cm = confusion_matrix(y_true, y_pred)
+    plt.figure(figsize=(10, 8))
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=test_generator.class_indices.keys(), yticklabels=test_generator.class_indices.keys())
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix for Test Set')
+    plt.show()
